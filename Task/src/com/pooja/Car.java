67771f64a4ff11ec0d7d01bfb40e4098d35e50f3
poojakukreti <59013969+poojakukreti54@users.noreply.github.com>
2021-06-26 22:19:06 +0530

    Add files via upload

---
+++ Task/src/com/pooja/Car.java
@@ -0,0 +1,59 @@
+package com.pooja;
+
+import com.pooja.Vehicle;
+
+public class Car extends Vehicle{
+	
+	private int noOfwheels;
+	private String color;
+	private String brand;
+	
+	/*
+	 1. To create two Java classes. Class 'Vehicle' and 'Car'. One of them will be an abstract class and the other one will extend the abstract class.
+	2. The abstract class will have two abstract functions - 'drive' and 'park'. And it will have data members: noOfWheels, colour, brand.
+	3. The child class will override the abstract functions and print the appropriate message as concrete implementation.
+
+
+	4. In the child class, assign values to the inherited data members via a parameterized constructor.
+	BONUS - In addition to the parameterized constructor, find a way to create an object of the implementing class without any parameters and to assign values via functions.
+
+	 */
+	
+	
+	
+	
+	public Car() {
+		super();
+		
+		
+	}
+
+	public Car(int noOfwheels, String color, String brand) {
+		super();
+		this.noOfwheels = noOfwheels;
+		this.color = color;
+		this.brand = brand;
+	}
+	
+	public void m1(){
+		System.out.println("noOfwheels: "+noOfwheels);
+		System.out.println("color: "+color);
+		System.out.println("brand: "+brand);
+
+	}
+	
+	
+	public void park() {
+		// TODO Auto-generated constructor stub
+		System.out.println("concrete park implementation");
+	}
+
+	public void drive() {
+		// TODO Auto-generated method stub
+		System.out.println("concrete drive implementation");
+	}
+	
+	Car obj = new Car(4,"red","benz");
+	obj.m1();
+
+}
---
+++ Task/src/com/pooja/Vehicle.java
@@ -0,0 +1,12 @@
+package com.pooja;
+
+public abstract class Vehicle {
+	
+	
+
+	public abstract void drive() ;
+	public abstract void park() ;
+
+	
+
+}
